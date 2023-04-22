c395ce66386e5860682758ddbaac682af13b4643
Kamil Chmielewski <kamyk962@gmail.com>
2023-04-22 13:59:12 +0200

    added user authentication to project

--- src/main/java/com/example/funeralbackend/auth/AuthController.java
+++ src/main/java/com/example/funeralbackend/auth/AuthController.java
@@ -8,6 +8,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,13 +25,17 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody LoginDto loginDto) {
+    public ResponseEntity<Object> login(@RequestBody LoginDto loginDto) {
         Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
                 loginDto.getUsername(), loginDto.getPassword()));
         SecurityContextHolder.getContext().setAuthentication(authentication);
-        return new ResponseEntity<>("Logged successfully!", HttpStatus.OK);
+        UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User user = User.builder()
+                .username(userDetails.getUsername())
+                .role(UserType.valueOf(userDetails.getAuthorities().stream().toList().get(0).getAuthority()))
+                .build();
+        return new ResponseEntity<>(user, HttpStatus.OK);
     }
-
     @PostMapping("/register")
     public User register(@RequestBody User user) {
         return userService.registeeUser(user);
--- src/main/java/com/example/funeralbackend/auth/SecurityConfig.java
+++ src/main/java/com/example/funeralbackend/auth/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.funeralbackend.auth;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -26,15 +27,12 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        http.csrf().disable()
-                .authorizeHttpRequests((authorize) ->
-                        //authorize.anyRequest().authenticated()
-                        authorize.requestMatchers("/api/**").permitAll()
-                                .requestMatchers("/api/auth/**").permitAll()
-                                .anyRequest().authenticated()
-
-                );
-
+        http
+                .csrf().disable()
+                .cors().and()
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/api/auth/**").permitAll()
+                        .anyRequest().authenticated()).httpBasic(Customizer.withDefaults());
         return http.build();
     }
 }
